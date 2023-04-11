304559086673e764833c28202ef3029a3a80ec41
FractalEcho <fractalalia@proton.me>
2023-04-11 12:06:24 -0400

    WalkToggle functionality

--- EyedleGame/Assets/Scripts/WalkToggle.cs
+++ EyedleGame/Assets/Scripts/WalkToggle.cs
@@ -5,23 +5,22 @@ using Tobii.Gaming;
 
 public class WalkToggle : MonoBehaviour
 {
+    public FollowPoint followComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    void OnEnable()
-    {
-        print(this.GetComponent<GazeAware>().HasGazeFocus ? "has gaze focus" : "no gaze focus");
+        TobiiAPI.Start(null);
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject focused = TobiiAPI.GetFocusedObject();
-        print(focused != null ? focused.name : "nothing is focused object");
-
-        print(this.GetComponent<GazeAware>().HasGazeFocus ? "has gaze focus" : "no gaze focus");
+        GameObject focusedObject = TobiiAPI.GetFocusedObject();
+        if (null != focusedObject)
+        {
+            print("The focused game object is: " + focusedObject.name + " (ID: " + focusedObject.GetInstanceID() + ")");
+            followComponent.isWalking = !followComponent.isWalking;
+        }
     }
 }
